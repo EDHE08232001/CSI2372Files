c0646d55e45eb449b700baf4cedaebe6bd037606
Edward He <ehe058@uottawa.ca>
2024-10-03 15:52:45 -0400

    update

--- LabAssignments/Assignment3_300176553/Ex1/myFile1a.cpp
+++ LabAssignments/Assignment3_300176553/Ex1/myFile1a.cpp
@@ -3,16 +3,50 @@
 #include <iostream>
 using namespace std;
 
-enum Color { club, diamond, spades, heart };
-enum Face { seven, eight, nine, ten, jack, queen, king, ace };
+enum Color
+{
+	club,
+	diamond,
+	spades,
+	heart
+};
+enum Face
+{
+	seven,
+	eight,
+	nine,
+	ten,
+	jack,
+	queen,
+	king,
+	ace
+};
 
-struct Card{
+struct Card
+{
 	Color color;
 	Face face;
 };
 
-int main(){
+int main()
+{
 	Card game[32];
-	//YOUR CODE HERE
-}
+	// YOUR CODE HERE
+	int index = 0; // track position in the deck
 
+	// Nested loops to generate all 32 cards by combining color and face
+	for (int c = club; c <= heart; ++c)
+	{
+		for (int f = seven; f <= ace; ++f)
+		{
+			game[index].color = static_cast<Color>(c);
+			game[index].face = static_cast<Face>(f);
+			index++; // move to the next position in the array
+		}
+	}
+
+	for (int i = 0; i < 32; i++)
+	{
+		cout << "Card " << i + 1 << ": Color = " << game[i].color << ", Face = " << game[i].face << endl;
+	}
+}
--- LabAssignments/Assignment3_300176553/Ex1/myFile1b.cpp
+++ LabAssignments/Assignment3_300176553/Ex1/myFile1b.cpp
@@ -3,8 +3,24 @@
 #include <iostream>
 using namespace std;
 
-enum Color { club, diamond, spades, heart };
-enum Face { seven, eight, nine, ten, jack, queen, king, ace };
+enum Color
+{
+	club,
+	diamond,
+	spades,
+	heart
+};
+enum Face
+{
+	seven,
+	eight,
+	nine,
+	ten,
+	jack,
+	queen,
+	king,
+	ace
+};
 
 struct Card
 {
@@ -15,20 +31,30 @@ struct Card
 typedef Card Hand[5];
 
 /* testPair() function*/
-bool testPair(const Hand& h)
+bool testPair(const Hand &h)
 {
-	//YOUR CODE  
-}
+	// YOUR CODE
+	// iterate over all pairs
+	for (int i = 0; i < 5; ++i)
+	{
+		for (int j = i + 1; j < 5; ++j)
+		{
+			// check if two cards have the same face value
+			if (h[i].face == h[j].face)
+			{
+				return true; // When a pair is found
+			}
+		}
+	}
 
+	return false; // no pairs are found
+}
 
 /*Example of main()*/
 int main()
 {
-	bool testPair(const Hand&);
-	Hand myHand = { {club,nine}, {diamond, eight}, {spades, ace}, {spades, jack}, {club, ace} };
+	bool testPair(const Hand &);
+	Hand myHand = {{club, nine}, {diamond, eight}, {spades, ace}, {spades, jack}, {club, ace}};
 	cout << "\nI have at least: " << testPair(myHand) << " pair" << endl;
 	return 0;
 }
-
-
-
