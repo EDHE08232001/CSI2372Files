47e8bc5c85b059ebf514734927ab4e79632d18f6
Teodora Vukojevic <83979259+teodoravuk22@users.noreply.github.com>
2024-10-28 18:32:08 -0400

    Updating myFile1b.h

--- LabAssignments/Assignment4_300176553/Ex1/myFile1b.h
+++ LabAssignments/Assignment4_300176553/Ex1/myFile1b.h
@@ -11,10 +11,50 @@ class Student {
 	int* List_grades;
 	Course** List_courses;
 public:
-	Student(int, int); 	//constructor
-	~Student(); 		//destructor
-	double average();
-	int totalHours();
-	bool addCourse(Course*, int);
+	// Constructor
+	Student(int iD, int maxNumCourses) : numID(iD), nbCourses(0), maxCourses(maxNumCourses){
+		List_courses = new Course*[maxNumCourses];
+		List_grades = new int[maxNumCourses];
+	}
+	
+	// Destructor
+	~Student(){
+		delete[] List_courses;
+		delete[] List_grades;
+	}
+	
+	//Calculating the average grade of the student
+	double average() const{
+		if (nbCourses == 0) return 0.0;
+		int totalNumGrades = 0;
+		
+		for (int t = 0; t < nbCourses; t++){
+			totalNumGrades = totalNumGrades + List_grades[t];
+		}
+		
+		return static_cast<double>(totalNumGrades) / nbCourses;
+	}
+	
+	// Calculating the total hours of all courses that the student is taking
+	int totalHours() const {
+		int hrs = 0;
+		
+		for(int t = 0; t < nbCourses; t++){
+			hrs = hrs + List_courses[t]->getHours();
+		}
+		
+		return hrs;	
+	}
+	
+	// Adding the course and the corresponding grade to the student's course list
+	bool addCourse(Course* courseCode, int gradeOfCourse){
+		if(nbCourses < maxCourses){
+			List_courses[nbCourses] = courseCode;
+			List_grades[nbCourses] = gradeOfCourse;
+			nbCourses++;
+			return true;
+		}
+		
+		return false; //If course list is full
+	}
 };
-
